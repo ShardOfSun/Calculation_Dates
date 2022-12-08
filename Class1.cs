3a9779dcf99fa68064f87ab7f92f188a11b39274
Ivan Churilov <ivanvrn2003@gmail.com>
2022-12-08 19:29:57 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace MTP_Sem3_Lab3_Class_Date
+{
+    class Date
+    {
+        int day, month, year;
+
+        public Date(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        public int DaysInMonth(int month, int year)
+        {
+            int count = -1;
+
+            //Если февраль, то проверяем високосность
+            if (month == 2)
+            {
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) count = 29;
+                else count = 28;
+            }
+            //До июля (включительно) чётные - 30 дней, нечётные - 31
+            else if (month < 8)
+            { 
+                if (month % 2 == 0) count = 30;
+                else count = 31;
+            }
+            //После августа (включительно) нечётные - 30 дней, чётные - 31
+            else if (month >= 8)
+            {
+                if (month % 2 == 0) count = 31;
+                else count = 30;
+            }
+
+            return count;
+        }
+
+        public bool DateCheck(int day, int month,int year)
+        {
+            //Проверяем год
+            if (year <= 0) return false;
+
+            //Проверяем кол-во месяцев
+            if (month <= 0 || month > 12) return false;
+
+            //Проверяем кол-во дней
+            if (day > 0 && day < DaysInMonth(month, year)) return true;
+            else return false;
+        }
+
+        public int DaysFromAge1(int day, int month, int year)
+        {
+            int count;
+            
+            //Прибавляем дни на основе кол-ва лет
+            count = 365 * (year - 1);
+
+            //Учитываем високосные года
+            count -= year / 100;
+            count += year / 400;
+            count += year / 4;
+
+            //Прибавляем дни текущего года
+            int i;
+            for (i = 1; i < month; i++)
+            {
+                count += DaysInMonth(i, year);
+            }
+
+            //Прибавляем дни текущего месяца
+            count += day;
+
+            return count;
+        }
+
+        public int DayOfWeek(int day, int month, int year)
+        {
+            int d;
+            //Расчёт дня недели на основе знания кол-ва дней с начала времён
+            d = DaysFromAge1(day, month, year) % 7;
+            //Сдвигаем, если год високосный
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                if (d == 0) d = 6;
+                else d--;
+            }
+            //Сдвигаем, чтобы 0,1,...,6 соответсвовали mn,td,...,st
+            if (d == 0) d = 6;
+            else d--;
+
+            return d;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MTP_Sem3_Lab3_Class_Date
+{
+    enum DayWeek { mn, td, wd, thd, fd, std, sd };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дату задаём рандомно (включая некорректные даты)
+            Random r = new Random();
+            int year = (r.Next() % 10000);
+            int month = (r.Next() % 15);
+            int day = (r.Next() % 36);
+            
+            //Объявляем объект класса
+            Date d = new Date(day, month, year);
+            
+            //Выводим получившуюся дату
+            Console.WriteLine("Date: " + day + "." + month + "." + year + "\n");
+
+            //Проверяем, корректность даты
+            if (d.DateCheck(day, month, year))
+            {
+                Console.WriteLine("Data is correct.\n");
+                //Демонстрируем методы класса
+                Console.WriteLine("Days in this month: " + d.DaysInMonth(month, year));
+                Console.WriteLine("Days from 01.01.1: " + d.DaysFromAge1(day, month, year));
+                //Выводим день недели, используя перечисление (по индексу, возвращаемому методом)
+                DayWeek dw = (DayWeek)d.DayOfWeek(day, month, year);
+                Console.WriteLine("Day of the week: " + dw + "\n");
+            }
+            //Если сгенерированная дата ошибочна, не можем демонстрировать методы
+            else Console.WriteLine("Data is wrong.\n");
+
+            //Задержка консоли
+            Console.WriteLine("Press any key to close.\n");
+            Console.ReadKey(true);
+        }
+    }
+}
